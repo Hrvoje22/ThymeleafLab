1eab40e090fb0885d4f32ba49e3cf5137068c3b1
Hrvoje <hbrkljacic@gmail.com>
2022-10-15 23:40:07 -0400

    createProduct

--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/ProductController.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.cydeo.spring05thymeleaf.controller;
 
+import com.cydeo.spring05thymeleaf.model.Product;
 import com.cydeo.spring05thymeleaf.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
@@ -14,7 +17,7 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping
+    @GetMapping("/list")
     public String retrieveAllList(Model model){
 
         model.addAttribute("productList",productService.listProduct());
@@ -22,6 +25,27 @@ public class ProductController {
 
         return "product/list";
     }
+
+
+    @GetMapping("/create-form")
+    public String getCreateProductForm(Model model){
+
+        model.addAttribute("product",new Product());
+
+
+        return "product/create-product";
+    }
+
+    @PostMapping("/create-product")
+    public String createProduct(@ModelAttribute("product") Product product){
+
+        productService.productCreate(product);
+
+
+        return "redirect:/list";
+    }
+
+
 
 
 }
--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/service/impl/ProductServiceImpl.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/service/impl/ProductServiceImpl.java
@@ -19,14 +19,17 @@ public class ProductServiceImpl implements ProductService {
     }
     @Override
     public boolean productCreate(Product product){
-        // todo implement method
-        return true;
+
+        product.setId(UUID.randomUUID());
+
+        return productRepository.save(product);
+
     }
 
     @Override
     public List<Product> listProduct() {
-        // todo implement method
-        return new ArrayList<>();
+
+        return productRepository.findAll();
     }
 
     @Override
